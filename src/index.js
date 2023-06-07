abcea6f964217c0041b951ae0644447352ab8478
Vitorakavitor <49196415+Vitorakavitor@users.noreply.github.com>
2023-06-06 23:29:45 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Irriga from './Irriga'
-
-
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
